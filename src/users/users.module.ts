9b559b021d32709502646efbab44c20318d3427e
Marco Monteiro <monteirochaincrypto@gmail.com>
2024-08-07 14:53:47 -0300

    estudo nest

---
+++ src/users/users.module.ts
@@ -0,0 +1,11 @@
+import { Module } from '@nestjs/common';
+import { UsersController } from './users.controller';
+import { UsersService } from './users.service';
+
+@Module({
+  imports: [TypeOrmModule.forFeature([UserEntity])],
+  providers: [UsersService],
+  exports: [UsersService],
+  controllers: [UsersController],
+})
+export class UsersModule {}
